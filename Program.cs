5534c901a97885e6a8e6f08d984dbffc00bccbaf
Vladyslav Shatulskyi <vladyslav.shatulskyi@stryker.com>
2022-01-16 22:37:09 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace InstaPhoto
+{
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Windows only")]
+    public static class Program
+    {
+        private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static void Main()
+        {
+            var files = Directory.EnumerateFiles(Path.Combine(BaseDirectory, "in")).ToArray();
+            
+            for (var i = 0; i < files.Length; i+=2)
+            {
+                using var file1 = Image.FromFile(files.ElementAt(i));
+                using var file2 = Image.FromFile(files.ElementAt(i+1));
+                using var result = new Bitmap(1750, 1166);
+                using var g = Graphics.FromImage(result);
+                
+                g.FillRegion(new SolidBrush(Color.DarkGray), new Region(new Rectangle(0, 0, 1750, 1166)));
+                g.DrawImage(file1, 0, 71, 866, 1024);
+                g.DrawImage(file2, 884, 71, 866, 1024);
+
+                result.Save(Path.Combine(BaseDirectory, $"{i}{i+1}.png"));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # InstaPhoto
+
+- Place photos madi in [mySquare](https://play.google.com/store/apps/details?id=kernelmachine.mysquare&hl=ru&gl=US) to `in` folder and start `InstaPhoto.exe`. 
+- Photo to print on 10x15 paper will be created in `out` folder.
